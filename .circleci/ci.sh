1ae44d5d458078672eb18aaf2b0fe57efd1a4de6
André Bauer <andre.bauer@kiwigrid.com>
2018-12-29 16:01:59 +0100

    removed travis

--- .circleci/ci.sh
+++ .circleci/ci.sh
@@ -114,7 +114,7 @@ if [ "${LINT}" == "yes" ]; then
   lint
 fi
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${INSTALL}" == "yes" ]; then
+if [ -n "${CIRCLE_PR_NUMBER}" ] && [ "${INSTALL}" == "yes" ]; then
   for K8S_VERSION in "${KUBERNETES_VERSIONS[@]}"; do
     echo -e "\\nTesting in Kubernetes ${K8S_VERSION}\\n"
     main
