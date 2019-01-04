4c941f87122a17fce8b6d3f43402e89b9b3f2603
André Bauer <andre.bauer@kiwigrid.com>
2019-01-04 13:41:04 +0100

    removed tmpdir usage from lint scripts

--- .circleci/lint-scripts.sh
+++ .circleci/lint-scripts.sh
@@ -7,13 +7,7 @@ set -o errexit
 
 CONFIG_DIR="./.circleci"
 
-TMP_FILE="$(mktemp)"
-
-find "${CONFIG_DIR}" -type f -name "*.sh" > "${TMP_FILE}"
-
 while read -r FILE; do
   echo lint "${FILE}"
   shellcheck -x "${FILE}"
-done < "${TMP_FILE}"
-
-rm "${TMP_FILE}"
+done < <(find "${CONFIG_DIR}" -type f -name "*.sh" > "${TMP_FILE}")
