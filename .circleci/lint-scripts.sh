4a939dd3b3f0bc409ef993c8c3c3fcdb414f26a8
André Bauer <andre.bauer@kiwigrid.com>
2019-01-04 13:50:36 +0100

    back to tmp dir version to be sh compatible

--- .circleci/lint-scripts.sh
+++ .circleci/lint-scripts.sh
@@ -6,8 +6,13 @@
 set -o errexit
 
 CONFIG_DIR="./.circleci"
+TMP_FILE="$(mktemp)"
+
+find "${CONFIG_DIR}" -type f -name "*.sh" > "${TMP_FILE}"
 
 while read -r FILE; do
   echo lint "${FILE}"
   shellcheck -x "${FILE}"
-done < <(find "${CONFIG_DIR}" -type f -name "*.sh" > "${TMP_FILE}")
+done < "${TMP_FILE}"
+
+rm "${TMP_FILE}"
