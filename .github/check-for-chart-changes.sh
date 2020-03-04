a5a1c1bd0fc1955a4473430c97e44d5dec38fb3f
André Bauer <monotek23@gmail.com>
2020-03-04 23:53:42 +0100

    added kubeval to github action
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 echo "Check for chart changes to speedup ci..."
 
-CHART_CHANGES="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts)"
+CHART_CHANGES="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- zammad)"
 
 if [ -z "${CHART_CHANGES}" ]; then
   echo -e "\n\n Error! No chart changes detected! Exiting... \n"
