4727448792940169bf038be44c02877686921408
André Bauer <monotek23@gmail.com>
2020-03-04 23:59:43 +0100

    added kubeval to github action
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -8,7 +8,13 @@ set -o pipefail
 
 CHART_DIRS="$(git diff --name-only remotes/origin/master | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 
-for CHART_DIR in ${CHART_DIRS};do 
+helm repo add bitnami https://charts.bitnami.com
+helm repo add elastic https://helm.elastic.co
+
+for CHART_DIR in ${CHART_DIRS};do
+  echo "helm dependency build..."
+  helm dependency build "${CHART_DIR}"
+
   echo "kubeval(idating) ${CHART_DIR##charts/} chart..."
   helm template "${CHART_DIR}" | docker run -i kiwigrid/gcloud-kubectl-helm:latest kubeval
 done
