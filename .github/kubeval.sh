de1875e5fb998b2db65fcf210f0f57f13bac18b0
André Bauer <monotek@users.noreply.github.com>
2020-03-05 00:31:40 +0100

    Update kubeval.sh

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -8,7 +8,7 @@ set -o pipefail
 
 # needed for github actions as home dir would be /github/home/ otherwise
 HOME="/home/gkh"
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- zammad | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 
 helm repo add kiwigrid https://kiwigrid.github.io/
 helm repo add stable https://kubernetes-charts.storage.googleapis.com/
