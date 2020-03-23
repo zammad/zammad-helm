49e1a840762ae3a6134065c46d8cf6a85b91cec2
André Bauer <monotek23@gmail.com>
2020-03-23 16:18:28 +0100

    debug checkout

--- .github/check-for-chart-changes.sh
+++ .github/check-for-chart-changes.sh
@@ -6,11 +6,14 @@
 set -o errexit
 set -o pipefail
 
+set -x
 git branch -a
+git rev-parse --abbrev-ref HEAD
 
 echo "Check for chart changes to speedup ci..."
 
-CHART_CHANGES="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- zammad)"
+#CHART_CHANGES="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- zammad)"
+CHART_CHANGES="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" master -- zammad)"
 
 if [ -z "${CHART_CHANGES}" ]; then
   echo -e "\n\n Error! No chart changes detected! Exiting... \n"
