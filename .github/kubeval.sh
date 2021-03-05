93b03f5b8d691464bba02711ad03b02172cdb098
André Bauer <monotek@users.noreply.github.com>
2021-03-05 14:33:00 +0100

    use nonroot container (#82)
    * use nonroot container
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * disable JSCPD linter
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * update k8s versions for kubeval and kind
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * remove helm install from kubeval script
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix rsync
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added securityContext
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix whitespaces
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix whitespaces
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * fix init
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added --omit-dir-times to rsync
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added --no-perms --no-owner to rsync
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * remove verbose flag from rsync
    
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -7,14 +7,8 @@ set -o errexit
 set -o pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- zammad | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-HELM_VERSION="v3.3.1"
 KUBEVAL_VERSION="0.15.0"
-SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
-
-# install helm
-curl --silent --show-error --fail --location --output get_helm.sh https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get
-chmod 700 get_helm.sh
-./get_helm.sh --version "${HELM_VERSION}"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
