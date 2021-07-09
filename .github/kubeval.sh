f87056304d13fbfdd117325caadfe83541806126
André Bauer <andre.bauer@staffbase.com>
2021-07-09 12:39:46 +0200

    fix kubeval
    Signed-off-by: André Bauer <andre.bauer@staffbase.com>

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -7,7 +7,7 @@ set -o errexit
 set -o pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- zammad | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/main/"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
@@ -19,5 +19,5 @@ for CHART_DIR in ${CHART_DIRS};do
   helm dependency build "${CHART_DIR}"
 
   echo "kubeval(idating) ${CHART_DIR##charts/} chart..."
-  helm template "${CHART_DIR}" | kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  helm template "${CHART_DIR}" | kubeval --strict --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
